00ec1194d81784c6c37e50779ba7e201ac949ffd
JefferyMatthewProdian <jeffery.matthew@prodian.co.in>
2026-08-12 22:10:46 +0530

    Second Largest Solution

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,13 +1,36 @@
+
+import java.util.*;
+
 public class SecondLargest{
-    public static void main(String[] args){
-        int [] arr = {10,5,8,20,15,20};
-        int first = 0;
-        int second = 0;
-        for i in range(arr.length){
-            if (first<i){
-                first = i;
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 8, 20, 15, 20};
+        int max = Integer.MIN_VALUE;
+        int secondMax =  Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
             }
-            if(second)
+            if (arr[i] < max && arr[i] > secondMax) {
+                secondMax = arr[i];
+            }
         }
+        System.out.println("This is max "+max+"  This is second max "+secondMax);
     }
 }
+
+
+//attempt 1
+//public class SecondLargest{
+//    public static void main(String[] args){
+//        int [] arr = {10,5,8,20,15,20};
+//        int max = Arrays.stream(arr).max().getAsInt();
+//        int secondMax = 0;
+//        for (int i:arr){
+//            if(secondMax<i && i!=max){
+//                secondMax = i;
+//            }
+//        }
+//        System.out.println(secondMax);
+//    }
+//}
