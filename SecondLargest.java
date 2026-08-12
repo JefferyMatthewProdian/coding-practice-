3d6d56d5e2e582e84f5a61dc03ada3ce5eafb27c
JefferyMatthewProdian <jeffery.matthew@prodian.co.in>
2026-08-12 14:56:58 +0530

    SecondLargest

---
+++ SecondLargest.java
@@ -0,0 +1,13 @@
+public class SecondLargest{
+    public static void main(String[] args){
+        int [] arr = {10,5,8,20,15,20};
+        int first = 0;
+        int second = 0;
+        for i in range(arr.length){
+            if (first<i){
+                first = i;
+            }
+            if(second)
+        }
+    }
+}
