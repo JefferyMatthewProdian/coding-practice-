8b1ccd957974378510c374ce2418d13c0cfdcca4
JefferyMatthewProdian <jeffery.matthew@prodian.co.in>
2026-08-17 21:50:16 +0530

    17th Aug

---
+++ BuyAndSellStock.java
@@ -0,0 +1,6 @@
+public class BuyAndSellStock {
+    public static void main(String[] args) {
+        int [] arr = {7, 1, 5, 3, 6, 4};
+
+    }
+}
---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ReverseString {
+    public static void main(String[] args) {
+        String s = "Hello";
+        char [] arr = s.toCharArray();
+        int i =0;
+        int j = arr.length-1;
+        while ( i<j ) {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.println(new String(arr));
+    }
+}
